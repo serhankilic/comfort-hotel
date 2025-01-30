15d3ecee5186304b76196374de3a1984cee87f2c
Serhan Kılıç <sserhankilic@gmail.com>
2025-01-30 20:51:41 +0300

    updated files

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: [['@nuxtjs/google-fonts', {
+    families: {
+      Roboto: true,
+      Inter: [400, 700],
+      'Josefin+Sans': true,
+      Lato: [100, 300],
+      Raleway: {
+        wght: [100, 400],
+        ital: [100]
+      },
+    }
+  }], '@nuxt/icon'],
 })
