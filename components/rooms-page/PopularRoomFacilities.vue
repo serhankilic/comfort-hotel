7393ccfdf726da3782fd2ea00c9073a5fe8af675
Serhan Kılıç <sserhankilic@gmail.com>
2025-02-16 23:49:36 +0300

    rooms page customer area left for responsive

--- components/rooms-page/PopularRoomFacilities.vue
+++ components/rooms-page/PopularRoomFacilities.vue
@@ -134,11 +134,445 @@
                     .more {
                         font-size: 18px;
                         color: #ae0505;
+                        margin: 0;
                     }
                     .list-text {
                         font-size: 18px;
                         color: whitesmoke;
                         font-weight: 400;
+                    }
+                }
+            }
+        }
+    }
+}
+@media only screen and (max-width: 1536px) {
+    .room-facilities-container {
+        width: 100%;
+        height: 100%;
+        .room-facilities-content {
+            max-width: 1300px;
+            padding: 100px 50px;
+            display: flex;
+            flex-direction: row;
+            column-gap: 120px;
+            justify-content: center;
+            align-items: center;
+            .left-side {
+                display: flex;
+                flex-direction: column;
+                justify-content: flex-start;
+                .small-title {
+                    font-size: 18px;
+                    color: #ae0505;
+                    font-weight: 500;
+                }
+                .big-title {
+                    font-family: 'Bona+Nova', serif;
+                    font-size: 38px;
+                    color: whitesmoke;
+                    padding-bottom: 30px;
+                }
+                .description {
+                    font-size: 16px;
+                    font-weight: 300;
+                    color: whitesmoke;
+                    max-width: 600px;
+                    padding-bottom: 30px;
+                }
+                .more {
+                    width: fit-content;
+                    padding-bottom: 5px;
+                    text-decoration: none;
+                    color: whitesmoke;
+                    font-family: 'Bona+Nova', serif;
+                    font-size: 18px;
+                    font-weight: 300;
+                    border-bottom: 1px solid #ae0505;
+                    transition: 0.3s all ease-in-out;
+                    opacity: 85%;
+                }
+                .more:hover {
+                    border-bottom: 1px solid dimgray;
+                    opacity: 1;
+                }
+            }
+            .right-side {
+                height: 100%;
+                display: flex;
+                justify-content: flex-start;
+                .list-content {
+                    padding: 0 0 20px;
+                    margin-top: 40px;
+                    display: grid;
+                    grid-template-rows: repeat(5, 1fr);
+                    grid-template-columns: repeat(2, 1fr);
+                    row-gap: 20px;
+                    column-gap: 50px;
+                    .list-item {
+                        display: flex;
+                        flex-direction: row;
+                        column-gap: 10px;
+                        justify-content: flex-start;
+                        align-items: center;
+                        .more {
+                            font-size: 16px;
+                            color: #ae0505;
+                        }
+                        .list-text {
+                            font-size: 16px;
+                            color: whitesmoke;
+                            font-weight: 400;
+                            white-space: nowrap;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+@media only screen and (max-width: 1280px) {
+    .room-facilities-container {
+        width: 100%;
+        height: 100%;
+        .room-facilities-content {
+            max-width: 1200px;
+            padding: 80px 40px;
+            display: flex;
+            flex-direction: row;
+            column-gap: 120px;
+            justify-content: center;
+            align-items: center;
+            .left-side {
+                display: flex;
+                flex-direction: column;
+                justify-content: flex-start;
+                .small-title {
+                    font-size: 16px;
+                    color: #ae0505;
+                    font-weight: 500;
+                }
+                .big-title {
+                    font-family: 'Bona+Nova', serif;
+                    font-size: 36px;
+                    color: whitesmoke;
+                    padding-bottom: 30px;
+                }
+                .description {
+                    font-size: 14px;
+                    font-weight: 300;
+                    color: whitesmoke;
+                    max-width: 600px;
+                    padding-bottom: 30px;
+                }
+                .more {
+                    width: fit-content;
+                    padding-bottom: 5px;
+                    text-decoration: none;
+                    color: whitesmoke;
+                    font-family: 'Bona+Nova', serif;
+                    font-size: 16px;
+                    font-weight: 300;
+                    border-bottom: 1px solid #ae0505;
+                    transition: 0.3s all ease-in-out;
+                    opacity: 85%;
+                }
+                .more:hover {
+                    border-bottom: 1px solid dimgray;
+                    opacity: 1;
+                }
+            }
+            .right-side {
+                height: 100%;
+                display: flex;
+                justify-content: flex-start;
+                .list-content {
+                    padding: 0 0 20px;
+                    margin-top: 40px;
+                    display: grid;
+                    grid-template-rows: repeat(5, 1fr);
+                    grid-template-columns: repeat(2, 1fr);
+                    row-gap: 20px;
+                    column-gap: 50px;
+                    .list-item {
+                        display: flex;
+                        flex-direction: row;
+                        column-gap: 10px;
+                        justify-content: flex-start;
+                        align-items: center;
+                        .more {
+                            font-size: 16px;
+                            color: #ae0505;
+                        }
+                        .list-text {
+                            font-size: 14px;
+                            color: whitesmoke;
+                            font-weight: 400;
+                            white-space: nowrap;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+@media only screen and (max-width: 1024px) {
+    .room-facilities-container {
+        width: 100%;
+        height: 100%;
+        .room-facilities-content {
+            max-width: 1000px;
+            padding: 80px 40px;
+            display: flex;
+            flex-direction: row;
+            column-gap: 120px;
+            justify-content: center;
+            align-items: center;
+            .left-side {
+                display: flex;
+                flex-direction: column;
+                justify-content: flex-start;
+                .small-title {
+                    font-size: 16px;
+                    color: #ae0505;
+                    font-weight: 500;
+                }
+                .big-title {
+                    font-family: 'Bona+Nova', serif;
+                    font-size: 36px;
+                    color: whitesmoke;
+                    padding-bottom: 30px;
+                }
+                .description {
+                    font-size: 14px;
+                    font-weight: 300;
+                    color: whitesmoke;
+                    max-width: 600px;
+                    padding-bottom: 30px;
+                }
+                .more {
+                    width: fit-content;
+                    padding-bottom: 5px;
+                    text-decoration: none;
+                    color: whitesmoke;
+                    font-family: 'Bona+Nova', serif;
+                    font-size: 16px;
+                    font-weight: 300;
+                    border-bottom: 1px solid #ae0505;
+                    transition: 0.3s all ease-in-out;
+                    opacity: 85%;
+                }
+                .more:hover {
+                    border-bottom: 1px solid dimgray;
+                    opacity: 1;
+                }
+            }
+            .right-side {
+                height: 100%;
+                display: flex;
+                justify-content: flex-start;
+                .list-content {
+                    padding: 0 0 20px;
+                    margin-top: 40px;
+                    display: grid;
+                    grid-template-rows: repeat(5, 1fr);
+                    grid-template-columns: repeat(2, 1fr);
+                    row-gap: 15px;
+                    column-gap: 50px;
+                    .list-item {
+                        display: flex;
+                        flex-direction: row;
+                        column-gap: 10px;
+                        justify-content: flex-start;
+                        align-items: center;
+                        .more {
+                            font-size: 16px;
+                            color: #ae0505;
+                        }
+                        .list-text {
+                            font-size: 14px;
+                            color: whitesmoke;
+                            font-weight: 400;
+                            white-space: nowrap;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+@media only screen and (max-width: 920px) {
+    .room-facilities-container {
+        width: 100%;
+        height: 100%;
+        .room-facilities-content {
+            max-width: 850px;
+            padding: 80px 40px;
+            display: flex;
+            flex-direction: row;
+            column-gap: 120px;
+            justify-content: center;
+            align-items: center;
+            .left-side {
+                display: flex;
+                flex-direction: column;
+                justify-content: flex-start;
+                .small-title {
+                    font-size: 14px;
+                    color: #ae0505;
+                    font-weight: 500;
+                    padding-bottom: 10px;
+                }
+                .big-title {
+                    font-family: 'Bona+Nova', serif;
+                    font-size: 28px;
+                    color: whitesmoke;
+                    padding-bottom: 30px;
+                }
+                .description {
+                    font-size: 14px;
+                    font-weight: 300;
+                    color: whitesmoke;
+                    max-width: 600px;
+                    padding-bottom: 30px;
+                }
+                .more {
+                    width: fit-content;
+                    padding-bottom: 5px;
+                    text-decoration: none;
+                    color: whitesmoke;
+                    font-family: 'Bona+Nova', serif;
+                    font-size: 16px;
+                    font-weight: 300;
+                    border-bottom: 1px solid #ae0505;
+                    transition: 0.3s all ease-in-out;
+                    opacity: 85%;
+                }
+                .more:hover {
+                    border-bottom: 1px solid dimgray;
+                    opacity: 1;
+                }
+            }
+            .right-side {
+                height: 100%;
+                display: flex;
+                justify-content: flex-start;
+                .list-content {
+                    padding: 0 0 20px;
+                    margin-top: 40px;
+                    display: grid;
+                    grid-template-rows: repeat(5, 1fr);
+                    grid-template-columns: repeat(2, 1fr);
+                    row-gap: 15px;
+                    column-gap: 50px;
+                    .list-item {
+                        display: flex;
+                        flex-direction: row;
+                        column-gap: 10px;
+                        justify-content: flex-start;
+                        align-items: center;
+                        .more {
+                            font-size: 16px;
+                            color: #ae0505;
+                        }
+                        .list-text {
+                            font-size: 14px;
+                            color: whitesmoke;
+                            font-weight: 400;
+                            white-space: nowrap;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+@media only screen and (max-width: 788px) {
+    .room-facilities-container {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        justify-content: center;
+        .room-facilities-content {
+            max-width: 700px;
+            padding: 60px 40px;
+            display: flex;
+            flex-direction: column;
+            row-gap: 20px;
+            column-gap: 120px;
+            justify-content: center;
+            align-items: center;
+            .left-side {
+                display: flex;
+                flex-direction: column;
+                justify-content: flex-start;
+                padding-bottom: 20px;
+                .small-title {
+                    font-size: 14px;
+                    color: #ae0505;
+                    font-weight: 500;
+                    padding-bottom: 10px;
+                }
+                .big-title {
+                    font-family: 'Bona+Nova', serif;
+                    font-size: 28px;
+                    color: whitesmoke;
+                    padding-bottom: 30px;
+                }
+                .description {
+                    font-size: 14px;
+                    font-weight: 300;
+                    color: whitesmoke;
+                    max-width: 700px;
+                    padding-bottom: 30px;
+                }
+                .more {
+                    width: fit-content;
+                    padding-bottom: 5px;
+                    text-decoration: none;
+                    color: whitesmoke;
+                    font-family: 'Bona+Nova', serif;
+                    font-size: 16px;
+                    font-weight: 300;
+                    border-bottom: 1px solid #ae0505;
+                    transition: 0.3s all ease-in-out;
+                    opacity: 85%;
+                    margin: 0 auto;
+                }
+                .more:hover {
+                    border-bottom: 1px solid dimgray;
+                    opacity: 1;
+                }
+            }
+            .right-side {
+                width: 100%;
+                height: 100%;
+                display: flex;
+                justify-content: center;
+                .list-content {
+                    padding: 0;
+                    margin-top: 0;
+                    display: grid;
+                    grid-template-rows: repeat(5, 1fr);
+                    grid-template-columns: repeat(2, 1fr);
+                    row-gap: 15px;
+                    column-gap: 50px;
+                    .list-item {
+                        display: flex;
+                        flex-direction: row;
+                        column-gap: 10px;
+                        justify-content: flex-start;
+                        align-items: center;
+                        .more {
+                            font-size: 16px;
+                            color: #ae0505;
+                        }
+                        .list-text {
+                            font-size: 14px;
+                            color: whitesmoke;
+                            font-weight: 400;
+                            white-space: nowrap;
+                        }
                     }
                 }
             }
